39e2ade82964c2603c712e35b36e79635c3f537c
Manasvi-Gupta31 <manasvi10gupta@gmail.com>
2026-01-28 23:47:27 +0530

    Add files via upload

---
+++ 28jan.java
@@ -0,0 +1,17 @@
+//print a number using static keyword
+class 28jan {
+
+    // static variable
+    static int number = 10;
+
+    // static method
+    static void show() {
+        System.out.println("Number is: " + number);
+    }
+
+    public static void main(String[] args) {
+        // calling static method directly
+        show();
+    }
+}
+
