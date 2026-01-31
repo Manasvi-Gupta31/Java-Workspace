e1061afade474fdc294452c5039fab375292d34d
Manasvi-Gupta31 <manasvi10gupta@gmail.com>
2026-01-31 23:43:33 +0530

    Add files via upload

---
+++ duplicatecheck.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+class duplicatecheck {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter size of array: ");
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+
+        System.out.println("Enter array elements:");
+        for(int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        boolean duplicate = false;
+
+        for(int i = 0; i < n; i++) {
+            for(int j = i + 1; j < n; j++) {
+                if(arr[i] == arr[j]) {
+                    duplicate = true;
+                    break;
+                }
+            }
+        }
+
+        if(duplicate)
+            System.out.println("Array contains duplicate elements");
+        else
+            System.out.println("Array does not contain duplicate elements");
+
+        sc.close();
+    }
+
+}
