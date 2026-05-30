da14bbdee129fbf7974c545b0ca8ea1b34a6cee7
Manasvi-Gupta31 <manasvi10gupta@gmail.com>
2026-05-30 23:59:35 +0530

    Add files via upload

---
+++ PrimeNumbers_123.java
@@ -0,0 +1,19 @@
+// Find Prime Numbers Between 1 to 100.
+
+public class PrimeNumbers_123 {
+    public static void main(String[] args) {
+
+        for (int i = 2; i <= 100; i++) {
+
+            int count = 0;
+
+            for (int j = 1; j <= i; j++) {
+                if (i % j == 0)
+                    count++;
+            }
+
+            if (count == 2)
+                System.out.print(i + " ");
+        }
+    }
+}
