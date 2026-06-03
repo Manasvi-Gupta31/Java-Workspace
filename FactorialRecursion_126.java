96cf0d4d058ceddfe59326c38f2c1f87f68b885c
Manasvi-Gupta31 <manasvi10gupta@gmail.com>
2026-06-03 23:56:41 +0530

    Add files via upload

---
+++ FactorialRecursion_126.java
@@ -0,0 +1,13 @@
+// Find Factorial Using Recursion
+
+public class FactorialRecursion_126 {
+    static int factorial(int n) {
+        if (n == 0 || n == 1)
+            return 1;
+        return n * factorial(n - 1);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(factorial(5));
+    }
+}
